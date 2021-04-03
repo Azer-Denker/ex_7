3d31ea6790f367f66f73ba7139ecc358a4165338
Aziret Arlen <azerarlen312@gmail.com>
2021-04-03 16:04:07 +0600

    2 Этап

---
+++ source/webapp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from webapp.models import Poll, Choice
+
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question']
+
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['text', ]
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label='Search')
